ddc7475b6f213ca65225fb78f13aa8daa17f0a7a
Juan SB <juansb.dev@gmail.com>
2023-08-12 18:39:44 -0300

    ft - navbar buttons

--- clients/src/components/navBar/Nav.jsx
+++ clients/src/components/navBar/Nav.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Styles from './Nav.module.css'
 
 import { Button, Row, Col} from 'react-bootstrap';
-import { BsSearch } from 'react-icons/bs';
+import { BsSearch, BsPersonCircle, BsCart2} from 'react-icons/bs';
 
 
 export default function Nav() {
@@ -12,18 +12,29 @@ export default function Nav() {
         <Col className="logo col-2">
             <p>Logo</p>
         </Col>
-        <Col className="buscador col-6">
+        <Col className={`${Styles.col_search} col-6`}>
             <input className={Styles.inp_search} type="text" />
             <button className={Styles.btn_search}>
                 <BsSearch />
             </button>
+           
         </Col>
-        <Col className="section col-4">
-            <Col className="location">
-                <Button variant='secondary'>Login</Button>
-            </Col>
-            <Col className="login"></Col>
+        <Col className='col-1'>
+            <p>Ship to</p>
+        </Col>
+        <Col className="section col-3" >
+            <Row>
 
+                <button className={` col-6 ${Styles.btn_profile_custom}`}>
+                    <BsPersonCircle/>
+                    <p>login</p>
+                </button>
+                <button  className={`col-6 ${Styles.btn_cart_custom}`}>
+                    <BsCart2/>
+                    <p>cart</p>
+                </button>
+
+            </Row>
         </Col>
 
     </Row>
