88900f9c822e26c626bf0287b638e6145a6efb87
Juan SB <juansb.dev@gmail.com>
2023-08-14 19:55:44 -0300

    request

--- server/src/controllers/allProducts.js
+++ server/src/controllers/allProducts.js
@@ -1,6 +1,6 @@
 
 const getAllProducts = (req, res) =>{
-    return res.status(200).json({massage:"hola estamos en productos"})
+    return res.status(200).json({message:"hola estamos en productos"})
 }
 
 module.exports = {
